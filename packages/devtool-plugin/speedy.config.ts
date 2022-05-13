633e77dbda3c1255c35e034b97cd63ef81ae5a93
yanyuhao.joy <yanyuhao.joy@bytedance.com>
2022-05-13 13:07:18 +0800

    chore: rebase dev

---
+++ packages/devtool-plugin/speedy.config.ts
@@ -0,0 +1,21 @@
+import { builtinModules } from "module";
+import { defineConfig } from "@speedy-js/speedy-core";
+import { externalPlugin } from "@speedy-js/speedy-plugin-external";
+
+export default defineConfig({
+  output: {
+    path: "dist",
+    filename: "index",
+    format:"cjs",
+  },
+  input: {
+    index: "src/index.ts",
+  },
+  define: {
+    // navigator: JSON.stringify(undefined),
+  },
+  external: ["@speedy-js/devtool-ui"],
+  target: "es6",
+  minify: false,
+  plugins: [externalPlugin({ external: builtinModules })],
+});
